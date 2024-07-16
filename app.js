ec5fb8a10607afb9f7e803aeb4aa907f4ca7c71f
Sviatoslav <sviatos1706@gmail.com>
2024-07-16 13:32:58 +0300

    connected mongoDB

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express=require('express');
-
+const mongoose=require('mongoose');
 const app=express();
 
 const PORT=3000;
 
+mongoose.connect(`mongodb+srv://root:JS3ibNgyhbwzhCuI@cluster0.cx5ubjc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+.then(()=>{
+    console.log('mongoDB connected')
+})
+
 app.listen(PORT,()=>{
     console.log(`server work on port ${PORT}`);
 })
+// JS3ibNgyhbwzhCuI
